9a0376856d741a3afa2e8998c6f5cbeb6bb2c540
BlackHost-BlackHat <claudiuaverian@gmail.com>
2025-05-07 00:37:30 +0300

    Modificare RPS prin functii

--- javascript.js
+++ javascript.js
@@ -9,10 +9,10 @@ function getComputerChoice(){
 }
 
 //ask player for an input using prompt
-function getHumanChoice(){
-    let alegere=prompt("Care este alegerea ta (Rock/Paper/Scissor")
-    return alegere
-}
+// function getHumanChoice(){
+//     let alegere=prompt("Care este alegerea ta (Rock/Paper/Scissor")
+//     return alegere
+// }
 
 //verify who won the match or if a tie happened and display the result
 function score(computer,human){
@@ -34,40 +34,82 @@ if(computerScore > humanScore){ console.log(`You lose! ${computer} beats ${human
 
 }
 //putting the function together
-function playRound(){
-    let humanChoice=getHumanChoice().toLowerCase()
-    let computerChoice=getComputerChoice().toLowerCase()
-
+function playRound(humanChoice){
+let computerChoice=getComputerChoice().toLowerCase()
+ console.log(humanChoice)
     return score(computerChoice,humanChoice)
      
 }
+let hScore=0
+let cScore=0
+let eScore=0
+let k=0;
 
-//repeating playRound 5 times and display if player won or lose
-function playGame(){
-    let computerScore=0,humanScore=0,egalitate=0;
-    for(let i=0;i<5;i++){
-    let answer=playRound()
-        if(answer=="Player"){
-            humanScore++
-            console.log(`My score: ${humanScore}      Computer score: ${computerScore}        Draw: ${egalitate}`)
-            console.log("-----------------")
+const container=document.querySelector("#container")
+const div=document.createElement("div")
+const humanScore=document.createElement("p")
+const calculatorScore=document.createElement("p")
+const tie=document.createElement("p")
+const rezult=document.createElement("p")
+
+tie.textContent="Tie:"+eScore
+humanScore.textContent="Your score: "+hScore
+calculatorScore.textContent="Computer score: "+cScore
+container.appendChild(div)
+div.appendChild(humanScore)
+div.appendChild(calculatorScore)
+div.appendChild(tie)
+div.appendChild(rezult)
+
+// repeating playRound 5 times and display if player won or lose
+
+
+const butoane = document.querySelectorAll(".buton");
+
+butoane.forEach((buton) => { //parcuirge fiecare obiect cu clasa buton
+    buton.addEventListener("click", function() {
+        let rezultat;
+        if(k==0){
+            humanScore.textContent="Your score: "+hScore
+            calculatorScore.textContent="Computer score: "+cScore
+            tie.textContent="Tie:"+eScore
+        }
+        if (buton.classList.contains("Rock")) {
+            rezultat=playRound("rock");
+        } else if (buton.classList.contains("Paper")) {
+           rezultat= playRound("paper");
+        } else if (buton.classList.contains("Scissor")) {
+           rezultat= playRound("scissor");
+        }
+
+        if(rezultat=="Player"){
+            hScore++;k++
+            humanScore.textContent="Your score: "+hScore
+
         } 
-        else if(answer == "Computer") {
-            computerScore++
-            console.log(`My score: ${humanScore}      Computer score: ${computerScore}        Draw: ${egalitate}`)
-            console.log("-----------------")
+        else if(rezultat == "Computer") {
+            cScore++;k++
+            calculatorScore.textContent="Computer score: "+cScore
         }
         else {
-            egalitate++
-            console.log(`My score: ${humanScore}      Computer score: ${computerScore}        Draw: ${egalitate}`)
-            console.log("-----------------")
+            eScore++;k++
+            tie.textContent="Tie:"+eScore
         }
-    }
-    console.log("")
-    if(computerScore >humanScore) console.log("You lost!")
-    else if (computerScore <humanScore) console.log("You won!")
-    else console.log("Draw!")
+ 
+        if(k==5){
+          if(hScore>cScore){
+            rezult.textContent="You win!"
 
-}
+          }else if(hScore<cScore){
+            rezult.textContent="You lost!"
+          }else{
+            rezult.textContent="Is a tie!"
+          }
+            k = 0;hScore = 0;cScore = 0;eScore = 0;
 
-playGame()
+
+        }
+
+    });
+});
+
